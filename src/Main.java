8834f96aa5c1242942efc0bc580db4cef5b9f926
Adam <adamlobocki@gmail.com>
2019-06-07 01:58:30 +0200

    Zadanie zrobione

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Rower[] rower = new Rower[3];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            rower[i] = new Rower();
+            System.out.println("Podaj id roweru");
+            rower[i].setId(scanner.nextInt());
+            scanner.nextLine();
+            for (int j = 0; j != i; j++) {
+                while (rower[i].getId() == rower[j].getId()) {
+                    System.out.println("Podaj nowy rower ");
+                    rower[i].setId(scanner.nextInt());
+                    scanner.nextLine();
+                }
+            }
+            System.out.println("Podaj kolor kolor roweru ");
+            rower[i].setKolor(scanner.nextLine());
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(rower[i].getId() + " " + rower[i].getKolor());
+        }
+    }
+}
---
+++ src/Rower.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public class Rower {
+   private String kolor;
+   private int id;
+
+
+
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public void setKolor(String kolor) {
+        this.kolor = kolor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
